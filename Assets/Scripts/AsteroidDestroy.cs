85812dfead2cac23873f9f436ea3c968b5cc417b
meitalbarkana <chipimeital@gmail.com>
2016-09-15 23:40:04 +0300

    added explosions

---
+++ Assets/Scripts/AsteroidDestroy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidDestroy : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		/*if (other.tag == "GameBorder") {
+			Debug.Log ("Asteroid hit a border");
+			return;
+		}*/
+		Instantiate (explosion , transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			//Debug.Log ("player collision accured");
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
