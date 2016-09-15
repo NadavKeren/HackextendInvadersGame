be4b0aa6e1c66a8b2acedb076585e5a9f8adaaa5
drorsou <drorsu@walla.co.il>
2016-09-15 16:43:44 +0300

    Added player + game_borders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,50 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    [SerializeField]
+    private int speed;
+
+    private Rigidbody2D body;
+    private int moveX;
+    private int moveY;
+
 	// Use this for initialization
 	void Start () {
-	
+        body = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        /*if (Input.GetKey(KeyCode.UpArrow))
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y);
+            */
+        if (Input.GetKey(KeyCode.UpArrow))
+            moveY = 1;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            moveY = -1;
+        else moveY = 0;
+        if (Input.GetKey(KeyCode.RightArrow))
+            moveX = 1;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            moveX = -1;
+        else moveX = 0;
+        if (Input.GetKey(KeyCode.Space))
+            FireWeapon();
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(moveX * speed, moveY * speed);
+    }
+
+    void FireWeapon()
+    {
+
+    }
 }
