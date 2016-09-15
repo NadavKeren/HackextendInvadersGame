a4c458242b3ff00a1f029bf6595bbf5794a115d3
Nadav Keren <nadav.keren@gmail.com>
2016-09-15 20:47:08 +0300

    Added ShotsMover

---
+++ Assets/Scripts/ShotsMover.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotsMover : MonoBehaviour {
+    public float speed;
+
+	void Start () {
+        GetComponent<Rigidbody>().velocity = (new Vector3(0.0f,1.0f,0.0f)) * speed;
+	}	
+	
+}
