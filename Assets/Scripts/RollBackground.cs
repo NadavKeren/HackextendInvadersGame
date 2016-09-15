d36c62466c0091b5edce257e13ffde9f615fd036
meitalbarkana <chipimeital@gmail.com>
2016-09-15 16:46:39 +0300

    added background

---
+++ Assets/Scripts/RollBackground.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollBackground : MonoBehaviour {
+
+	public float scrollSpeed;
+	private Vector2 startPosition;
+	private Vector2 newPosition;
+
+	void Start () {
+		startPosition = transform.position;
+		newPosition = startPosition;
+	}
+
+	void Update () {
+		if(newPosition.y > -50)
+		{
+			newPosition = newPosition + (new Vector2 (0.0f, -1.0f)) * scrollSpeed * 0.001f ;
+			transform.position = newPosition;
+		}
+	}
+}
