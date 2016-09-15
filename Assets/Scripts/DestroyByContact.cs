b7c45a741ba195d7affbf41dc12250b6eb7e7098
meitalbarkana <chipimeital@gmail.com>
2016-09-15 17:57:15 +0300

    added astroid+script

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "GameBorder") {
+			return;
+		}
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
