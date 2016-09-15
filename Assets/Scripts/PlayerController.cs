bed47f75c51834e36937a9af15d805fd15c4e5db
drorsou <drorsu@walla.co.il>
2016-09-15 22:00:59 +0300

    Player controller combined with nadav

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,28 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    // Set Player speed
     [SerializeField]
     private int speed;    
 
+    // Set screen resolution
     [SerializeField]
     private int resWidth;
     [SerializeField]
     private int resHeight;
 
     private Rigidbody2D body;
+    private new SpriteRenderer renderer;
+
+    // Movement
     private int moveX;
     private int moveY;
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
+    // Shooting
     public GameObject mainShot;
     public GameObject secondaryShot;
     public Transform shotSpawn;
@@ -29,46 +40,24 @@ public class PlayerController : MonoBehaviour {
     private GameObject leftSecond;
     private GameObject rightSecond;
 
-    void fireMainWeapon()
-    {
-        nextMain = Time.time + fireRate;
-        Instantiate(mainShot, shotSpawn.position, Quaternion.Euler(0,0,0));
-    }
-    void fireSecondaryWeapon()
-    {
-        nextSecondary = Time.time + secondaryRate;
-        leftSecond = Instantiate(secondaryShot, leftWing.position, Quaternion.Euler(0, 0, 0)) as GameObject;
-        rightSecond = Instantiate(secondaryShot, rightWing.position, Quaternion.Euler(0, 0, 0)) as GameObject;
-        isSecondShot = true;
-    }
 	// Use this for initialization
 	void Start () {
-        body = GetComponent<Rigidbody2D>();
+
         Screen.SetResolution(resWidth, resHeight, true);
+
+        body = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+
+        minX = -9 + renderer.bounds.extents.x;
+        maxX = 9 - renderer.bounds.extents.x;
+        minY = -5 + renderer.bounds.extents.y;
+        maxY = 5 - renderer.bounds.extents.y;
+
         secondaryRate = 0.5f * fireRate;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        /*if (Input.GetKey(KeyCode.UpArrow))
-            transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y);
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y);
-            */
-        if (Input.GetKey(KeyCode.UpArrow))
-            moveY = 1;
-        else if (Input.GetKey(KeyCode.DownArrow))
-            moveY = -1;
-        else moveY = 0;
-        if (Input.GetKey(KeyCode.RightArrow))
-            moveX = 1;
-        else if (Input.GetKey(KeyCode.LeftArrow))
-            moveX = -1;
-        else moveX = 0;
         if (Input.GetKey(KeyCode.Space) && Time.time > nextMain)
             fireMainWeapon();
         if (Input.GetKey(KeyCode.LeftControl))
@@ -83,6 +72,35 @@ public class PlayerController : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        body.velocity = new Vector2(moveX * speed, moveY * speed);
-    }    
+        if (Input.GetKey(KeyCode.UpArrow))
+            moveY = 1;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            moveY = -1;
+        else moveY = 0;
+        if (Input.GetKey(KeyCode.RightArrow))
+            moveX = 1;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            moveX = -1;
+        else moveX = 0;
+
+        body.velocity = new Vector2(moveX, moveY) * speed;
+
+        Vector3 pos = body.position;
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        body.position = pos;
+    }
+
+    void fireMainWeapon()
+    {
+        nextMain = Time.time + fireRate;
+        Instantiate(mainShot, shotSpawn.position, Quaternion.Euler(0, 0, 0));
+    }
+    void fireSecondaryWeapon()
+    {
+        nextSecondary = Time.time + secondaryRate;
+        leftSecond = Instantiate(secondaryShot, leftWing.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        rightSecond = Instantiate(secondaryShot, rightWing.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        isSecondShot = true;
+    }
 }
